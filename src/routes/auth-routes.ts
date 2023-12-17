1078104c07dac624e6d9214e710aac3abfa3356b
jonathansaandev <jonathansaandev@gmail.com>
2023-12-17 17:44:18 -0300

    feat: Add login user controller and update auth routes

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { RegisterController } from '../controllers/auth/register-user-controller';
-import { CreateUserDTO } from '../controllers/user/DTOs/create-user-dto';
+import { LoginController } from '../controllers/auth/login-user-controller';
 import { ValidationMiddleware } from '../middlewares';
 
 export default (router: express.Router) => {
-  router.get('/auth/register', RegisterController);
+  router.post('/auth/register', ValidationMiddleware, RegisterController);
+  router.post('/auth/login', ValidationMiddleware, LoginController);
 };
--- src/routes/user-routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { ValidationMiddleware } from "../middlewares";
-import { CreateUserDTO } from "../controllers/user/DTOs/create-user-dto";
-import { createUserController } from "../controllers/user/create-user-controller";
-
-export default (router: express.Router) => {
-  router.post("/", ValidationMiddleware(CreateUserDTO), createUserController);
-};
