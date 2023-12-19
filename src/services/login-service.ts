6e80523fa88e8f13be4fb16ccca313e667813367
jonathansaandev <jonathansaandev@gmail.com>
2023-12-19 18:07:55 -0300

    feat: Add token refresh and expiration functionality

--- src/services/login-service.ts
+++ src/services/login-service.ts
@@ -9,18 +9,37 @@ export const loginService = async (username: string, password: string) => {
   const user = await prisma.user.findFirst({
     where: {
       username,
-      password,
     },
   });
-  
-  const equalPassword = bcrypt.compare(password, user.password);
+  if (!user) {
+    throw new Unauthorized("Invalid username or password");
+  }
 
+  const equalPassword = await bcrypt.compare(password, user.password);
   if (!equalPassword) {
     throw new Unauthorized("Invalid username or password");
   }
-  
+
   return user;
 };
 
-export const generateTokenService = async (username: string) =>
-  jvt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "7d" });
+export const generateTokenService = async (id: string) =>
+  jvt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+export const refreshTokenService = async (token: string) => {
+  const decodedToken = jvt.verify(token, process.env.JWT_SECRET) as { id: string };
+  const newToken = jvt.sign({ id: decodedToken.id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+  return newToken;
+};
+
+export const setTokenExpirationService = async (token: string) => {
+  const decodedToken = jvt.verify(token, process.env.JWT_SECRET) as { id: string, exp: number };
+
+  const expirationDate = new Date(decodedToken.exp * 1000);
+
+  return {
+    token: jvt.sign({ id: decodedToken.id }, process.env.JWT_SECRET, { expiresIn: "7d" }),
+    expirationDate: expirationDate.toISOString(),
+  };
+};
