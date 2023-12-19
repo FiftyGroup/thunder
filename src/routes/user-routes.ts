98b0399b57d1a1e028ec335bde5f060146d2aea1
unknown <juliodeveloper777@gmail.com>
2023-12-19 17:50:42 -0300

    feat: Added user-routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import { ValidationMiddleware, authMiddleware } from '../middlewares';
 import { findByUsername } from '../controllers/user/findByUsername';
-
 
 export default (router: express.Router) => {
   router.get('/user/:username', findByUsername);
