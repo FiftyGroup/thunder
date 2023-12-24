cc6ef594e847dc5b9741f7b979e434ef3e5cf966
unknown <juliodeveloper777@gmail.com>
2023-12-23 21:46:29 -0300

    feat: Route password reset

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import router from "./routes/index";
 import { errorHandler } from "./errors/error-handler";
 import { Producer } from "./utils/Producer";
 import { amqp } from "./amqp";
+
 export const app = express();
-export const MailProducer = new Producer("ACCOUNT_CREATED",amqp)
+export const MailProducer = new Producer("ACCOUNT_CREATED", amqp)
+
 app.use(json());
 app.use("/", router());
 app.use(errorHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 import config from './config/custom-environment-variables'
 
-const PORT = config.port || 7000;
+const PORT = config.port || 4000;
 app.listen(PORT, () => {
   console.log(`Server running at the address: http://localhost:${PORT}`);
 });
